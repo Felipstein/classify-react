b42265eb8e1f162ae1a7740883cf8737b0534dbf
Felipstein <luisfelipe-oliveira@outlook.com.br>
2024-04-09 21:18:10 -0300

    feat: implement fully and correct logic

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,43 @@
 export function buildClassNames(
   propClassName: string | Record<string | number, string> | undefined | null,
-  newDefaultKey?: string,
+  defaultKey?: string,
 ) {
-  if (
-    typeof propClassName !== 'string' &&
-    typeof propClassName !== 'object' &&
-    propClassName !== null
-  ) {
+  if (isInvalidClassName(propClassName)) {
     throw new TypeError(
       'className must be a string, object, undefined or null',
     );
   }
 
+  function getDefaultKey() {
+    if (defaultKey) {
+      return defaultKey;
+    }
+
+    if (!propClassName || typeof propClassName !== 'object') {
+      return undefined;
+    }
+
+    return Object.keys(propClassName)[0];
+  }
+
   function getClassName(classNameKey: string) {
-    return classNameKey;
+    if (typeof propClassName === 'string' && classNameKey === getDefaultKey()) {
+      return propClassName;
+    }
+
+    return typeof propClassName === 'object'
+      ? propClassName?.[classNameKey] ?? undefined
+      : undefined;
   }
 
   return getClassName;
+}
+
+function isInvalidClassName(className: unknown) {
+  return (
+    typeof className !== 'string' &&
+    typeof className !== 'object' &&
+    className !== null &&
+    className !== undefined
+  );
 }
